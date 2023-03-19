3bb11c58422ed013a221f940c563871970b4a2f7
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-19 21:25:20 +0300

    Order Pizza Now

--- app/models/restraunt.rb
+++ app/models/restraunt.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-    has_many :restaurant_pizzas
-    has_many :pizzas, through: :restaurant_pizzas
+    has_many :restaurant_pizza
+    has_many :pizza, through: :restaurant_pizza
 end
   
