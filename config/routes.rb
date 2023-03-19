aa34a1a9242c1a8c4336fe2be931886dd95bf2e3
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-19 21:28:16 +0300

    Order Pizza Now

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show, :destroy]
-  resources :pizzas, only: [:index]
-  resources :restaurant_pizzas, only: [:create]
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/:id', to: 'restaurants#show'
+  delete '/restaurants/:id', to: 'restaurants#destroy'
+  get '/pizzas', to: 'pizzas#index'
+  post '/restaurant_pizzas', to: 'restaurant_pizzas#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
